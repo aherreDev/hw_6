151d666382d49ca653fd0b262627af87598466bf
aherredev <aherredev@gmail.com>
2020-10-22 01:11:11 -0500

    Add some coderefactors

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ import Inventory from './models/Inventory.js'
 
 window.onload = () =>{
   $(document).ready(function(){
-    M.AutoInit();
-    $('input, textarea').on('focus',(e)=>{
-      console.log()
-      $(`label[for="${e.target.id}"]`).addClass("active")
-    })
-    $('input, textarea').on('blur',(e)=>{
-      if(e.target.value) return null
-      $(`label[for="${e.target.id}"]`).removeClass("active")
-    })
+    initMaterialize()
+
     new Inventory();
   });
+}
+
+const initMaterialize = () =>{
+  M.AutoInit();
+  $('input, textarea').on('focus',(e)=>{
+    $(`label[for="${e.target.id}"]`).addClass("active")
+  })
+  $('input, textarea').on('blur',(e)=>{
+    if(e.target.value) return null
+    $(`label[for="${e.target.id}"]`).removeClass("active")
+  })
 }
 
 
